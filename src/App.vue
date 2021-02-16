8c097300cc0660566fd66024b7bad5c9513d4bb2
Arthur <arthur.quaranta@ynov.com>
2021-02-16 18:40:31 +0100

    Add table and interface

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <Header first_link="www.google.fr" second_link="List.vue"/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <List />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import List from './components/List.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header,
+    List
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
